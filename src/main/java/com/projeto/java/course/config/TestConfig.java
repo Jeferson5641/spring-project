a6a5aac0f5baede4f1f466cc3d9950cbaee4d23f
Jeferson5641 <jefe.gomes154@gmail.com>
2024-04-16 08:45:40 -0300

    Service layer, component registration

--- src/main/java/com/projeto/java/course/config/TestConfig.java
+++ src/main/java/com/projeto/java/course/config/TestConfig.java
@@ -2,13 +2,12 @@ package com.projeto.java.course.config;
 
 
 import com.projeto.java.course.entities.User;
-import com.projeto.java.course.repositories.UserRepositories;
+import com.projeto.java.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 @Configuration
@@ -16,13 +15,13 @@ import java.util.Arrays;
 public class TestConfig implements CommandLineRunner {
 
     @Autowired
-    private UserRepositories userRepositories;
+    private UserRepository userRepository;
 
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        userRepositories.saveAll(Arrays.asList(u1, u2));
+        userRepository.saveAll(Arrays.asList(u1, u2));
     }
 }
--- src/main/java/com/projeto/java/course/repositories/UserRepository.java
+++ src/main/java/com/projeto/java/course/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package com.projeto.java.course.repositories;
 import com.projeto.java.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepositories extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
